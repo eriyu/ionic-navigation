f75bf12bbfddfba3ac30bf65be19d20502790668
拉拉醬 <lala.lee.goo@gmail.com>
2017-10-05 23:15:33 +0800

    新增 About Compoent

---
+++ src/components/about/about.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the AboutComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'about',
+  templateUrl: 'about.html'
+})
+export class AboutComponent {
+
+  text: string;
+
+  constructor() {
+    this.text = 'About';
+  }
+
+}
